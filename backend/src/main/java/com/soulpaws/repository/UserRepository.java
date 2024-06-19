132a3bd0958b07d387bd99057964eed330b3df8b
LugaresDev <lugaresdev@gmail.com>
2024-06-19 17:59:03 +0200

    Ajuste funcional inicial

--- backend/src/main/java/com/soulpaws/repository/UserRepository.java
+++ backend/src/main/java/com/soulpaws/repository/UserRepository.java
@@ -4,6 +4,6 @@ import com.soulpaws.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    User findByName(String name);
 }
 
