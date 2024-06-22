60dabc3ffd16a7d935b30a31a584acaddd6ea835
dinho-dev <68518134+dinho-dev@users.noreply.github.com>
2024-06-22 17:07:53 +0200

    Updated UserRepository

--- backend/src/main/java/com/soulpaws/repository/UserRepository.java
+++ backend/src/main/java/com/soulpaws/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.soulpaws.repository;
 import com.soulpaws.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByName(String name);
+    Optional<User> findByEmail(String email);
 }
 
