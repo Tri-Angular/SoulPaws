2cca13ed41f1c72088a2d4aac941190757a731f0
dinho-dev <68518134+dinho-dev@users.noreply.github.com>
2024-06-25 18:14:38 +0200

    Update pet-add component styles and features

--- frontend/src/app/components/pages/pets/pet-add/pet-add.component.ts
+++ frontend/src/app/components/pages/pets/pet-add/pet-add.component.ts
@@ -1,36 +1,12 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { PetService } from '../../../../services/api/pet.service';
-import { Pet } from '../../../../models/pet.model'; // Importa la interfaz Pet si aún no lo has hecho
 
 @Component({
   selector: 'app-pet-add',
+  standalone: true,
+  imports: [],
   templateUrl: './pet-add.component.html',
-  styleUrls: ['./pet-add.component.css']
+  styleUrl: './pet-add.component.css'
 })
 export class PetAddComponent {
-  pet: Pet = {  // Usa un objeto literal para inicializar pet
-    id: 0,
-    name: '',
-    age: 0,
-    shelterId: 0,
-    size: '',
-    gender: 'MALE',
-    breed: '',
-    image: '',
-    description: '',
-    createdAt: new Date(),
-    updatedAt: new Date()
-  };
 
-  constructor(
-    private petService: PetService,
-    private router: Router
-  ) {}
-
-  onSubmit(): void {
-    this.petService.createPet(this.pet).subscribe(() => {
-      this.router.navigate(['/pets']);
-    });
-  }
 }
