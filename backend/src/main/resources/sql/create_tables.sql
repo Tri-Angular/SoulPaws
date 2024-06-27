cb6b6718291fa3e3d51b01668f747f22078f030b
dinho-dev <68518134+dinho-dev@users.noreply.github.com>
2024-06-28 00:29:40 +0200

    Update SQL scripts for Breed entity and remove PetProfile references

--- backend/src/main/resources/sql/create_tables.sql
+++ backend/src/main/resources/sql/create_tables.sql
@@ -25,38 +25,37 @@ CREATE TABLE IF NOT EXISTS soulpaws.shelters (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS soulpaws.breeds (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    breed VARCHAR(100) NOT NULL,
+    species ENUM('CAT', 'DOG') NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS soulpaws.pets (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     age INT,
     shelter_id BIGINT,
+    breed_id BIGINT,
     size VARCHAR(20),
     gender ENUM('MALE', 'FEMALE') NOT NULL,
-    breed VARCHAR(100),
     image TEXT,
     description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (shelter_id) REFERENCES soulpaws.shelters(id)
-);
-
-CREATE TABLE IF NOT EXISTS soulpaws.pet_profiles (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    pet_id BIGINT,
     unique_features TEXT,
     availability_status ENUM('AVAILABLE_FOR_ADOPTION', 'IN_ADOPTION_PROCESS', 'ADOPTED') NOT NULL DEFAULT 'AVAILABLE_FOR_ADOPTION',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (pet_id) REFERENCES soulpaws.pets(id)
+    FOREIGN KEY (shelter_id) REFERENCES soulpaws.shelters(id),
+    FOREIGN KEY (breed_id) REFERENCES soulpaws.breeds(id)
 );
 
 CREATE TABLE IF NOT EXISTS soulpaws.adoption_requests (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT,
-    pet_profile_id BIGINT,
+    pet_id BIGINT,
     status ENUM('Pending', 'Approved', 'Rejected') NOT NULL DEFAULT 'Pending',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES soulpaws.users(id),
-    FOREIGN KEY (pet_profile_id) REFERENCES soulpaws.pet_profiles(id)
+    FOREIGN KEY (pet_id) REFERENCES soulpaws.pets(id)
 );
