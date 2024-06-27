688cd2d6fbbb621d2d84177e70a3835c00d497e3
dinho-dev <68518134+dinho-dev@users.noreply.github.com>
2024-06-27 04:23:25 +0200

    Update pet-search component with new style and filtering

--- frontend/src/app/components/pages/pets/pet-search/pet-search.component.ts
+++ frontend/src/app/components/pages/pets/pet-search/pet-search.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { PetService } from 'src/app/services/api/pet.service';
+import { Shelter } from 'src/app/models/shelter.model';
 
 @Component({
   selector: 'app-pet-search',
@@ -9,11 +11,29 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './pet-search.component.html',
   styleUrls: ['./pet-search.component.css']
 })
-export class PetSearchComponent {
-  searchQuery: string = '';
+export class PetSearchComponent implements OnInit {
+  @Output() filterEvent = new EventEmitter<any>();
 
-  search() {
-    // Логика поиска животных
-    console.log('Searching for pets with query:', this.searchQuery);
+  filterCriteria = {
+    searchText: '',
+    breed: '',
+    gender: '',
+    size: '',
+    age: ''
+  };
+
+  breeds: string[] = [];
+  shelters: Shelter[] = [];
+
+  constructor(private petService: PetService) {}
+
+  ngOnInit(): void {
+    this.petService.getAllPets().subscribe((pets: any[]) => {
+      this.breeds = [...new Set(pets.map(pet => pet.breed))];
+    });
+  }
+
+  onSearch(): void {
+    this.filterEvent.emit(this.filterCriteria);
   }
 }
