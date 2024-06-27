d67a1d4ec45f70002f525667ada1cc5222487f9e
dinho-dev <68518134+dinho-dev@users.noreply.github.com>
2024-06-27 04:18:28 +0200

    Update nav-bar component with new styles and HTML structure

--- frontend/src/app/components/layout/nav-bar/nav-bar.component.ts
+++ frontend/src/app/components/layout/nav-bar/nav-bar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { PetSearchComponent } from '../../pages/pets/pet-search/pet-search.component';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterModule, PetSearchComponent],
+  imports: [RouterModule],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
