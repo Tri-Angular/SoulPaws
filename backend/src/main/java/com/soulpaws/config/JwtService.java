2d25e0ddce952d75f807be949aebafa660f5e912
PabloES796 <pabloeussan@gmail.com>
2024-06-28 00:22:53 +0200

    Subida de imágenes a la base de datos

--- backend/src/main/java/com/soulpaws/config/JwtService.java
+++ backend/src/main/java/com/soulpaws/config/JwtService.java
@@ -17,8 +17,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
-    @Value("${security.jwt.secret-key}")
-    private String secretKey;
+    //@Value("${security.jwt.secret-key}")
+    //private String secretKey;
 
     @Value("${security.jwt.expiration-time}")
     private long jwtExpiration;
@@ -55,7 +55,7 @@ public class JwtService {
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                //.signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
@@ -75,14 +75,14 @@ public class JwtService {
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
-                .setSigningKey(getSignInKey())
+                //.setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
 
-    private Key getSignInKey() {
+    /*private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
-    }
+    }*/
 }
