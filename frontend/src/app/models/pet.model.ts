356305b1910ca3f6c99a2c7ed32ee4b8929a20a7
dinho-dev <68518134+dinho-dev@users.noreply.github.com>
2024-06-28 02:28:11 +0200

    Update Pet model and add Breed model

--- frontend/src/app/models/pet.model.ts
+++ frontend/src/app/models/pet.model.ts
@@ -1,13 +1,15 @@
+import { Breed } from './breed.model';
+
 export interface Pet {
-    id: number;
-    name: string;
-    age: number;
-    shelterId: number;
-    size: string;
-    gender: 'MALE' | 'FEMALE';
-    breed: string;
-    image: string;
-    description: string;
-    createdAt: Date;
-    updatedAt: Date;
+  id: number;
+  name: string;
+  age: number;
+  shelterId: number;
+  size: string;
+  gender: 'MALE' | 'FEMALE';
+  breed: Breed;
+  image: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
